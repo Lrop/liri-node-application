7b07fdd55a66b0bc9947c87990fb6a0d29be846b
Lrop <Lagropner04@gmail.com>
2019-07-17 17:08:53 -0600

    saving progess

--- liri.js
+++ liri.js
@@ -26,7 +26,7 @@ UserInputs (appInput, appSearch);
 function UserInputs(appInput, appSearch) {
     switch (appInput) {
         case "spotify-this-song":
-            getSpotify(appSearch);
+            getMeSpotify(appSearch);
             break;
 
         case "concert-this":
@@ -40,41 +40,131 @@ function UserInputs(appInput, appSearch) {
         case "do-what-it-says":
             doingSomething();
             break;
+
+
         default:
-            console.log("Error. Please enter vailid call back: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+            console.log('-------------------------------------')
+            console.log("Error. Please enter vailid call back: \n  concert-this \n  spotify-this-song \n  movie-this \n  do-what-it-says")
+            console.log('-------------------------------------')
         }
 
     }
 
    
+    // function getConcertInfo(appSearch){
+    //     var queryUrl = "https://rest.bandsintown.com/artists/" + appSearch + "/events?app_id=codingbootcamp";
+    //     console.log(queryUrl)
+    //     request(queryUrl, function(error, response, offers) {
+    //     // If the request is successful
+    //     if (appSearch === appSearch) {
+    //         var concerts = JSON.parse(offers);
+    //         for (var i = 0; i < concerts.length; i++) {  
+    //             console.log("-----------------------------------------------------------------------------")
+    //             console.log("    The Artist " + appSearch + " Will be performing on the following dates and venues:")
+    //             console.log("-----------------------------------------------------------------------------")
+    //             console.log("                ***--------EVENT INFO--------***");  
+    //             fs.appendFileSync("random.txt", "**********EVENT INFO*********\n");
+    //             console.log(i);
+    //             fs.appendFileSync("random.txt", i+"\n");
+    //              console.log("              | Ticket Status: " + concerts[i].offers.type + " |");
+    //             fs.appendFileSync("random.txt", i+"\n");
+    //             console.log("              | Name of the Venue: " + concerts[i].venue.name + " |");
+    //             fs.appendFileSync("random.txt",      "Name of the Venue: " + concerts[i].venue.name+"\n");
+    //             console.log("              | Venue Location: " +  concerts[i].venue.city + " |");
+    //             fs.appendFileSync("random.txt",      "Venue Location: " +  concerts[i].venue.city+"\n");
+    //             console.log("              | Date of the Event: " +  concerts[i].datetime + " |");
+    //             fs.appendFileSync("random.txt",      "Date of the Event: " +  concerts[i].datetime+"\n");
+    //             console.log("              | Description of the Event: " +  concerts[i].description + " |");
+    //             fs.appendFileSync("random.txt",      "Description of Event: " +  concerts[i].description+"\n");
+                
+    //             console.log("                                                                       ");
+    //             fs.appendFileSync("random.txt", "*****************************"+"\n");
+    //         }
+    //     } else{
+    //       console.log('Error occurred.');
+    //     }
+    // });
+    
+    // }
+
+    
     function getConcertInfo(appSearch){
         var queryUrl = "https://rest.bandsintown.com/artists/" + appSearch + "/events?app_id=codingbootcamp";
-        request(queryUrl, function(error, response, body) {
-        // If the request is successful
-        if (appSearch === appSearch) {
-            var concerts = JSON.parse(body);
-            for (var i = 0; i < concerts.length; i++) {  
+
+        console.log(queryUrl);
+
+        axios.get(queryUrl).then(
+            function(response) {
+              var jsonData = response.data;
+        console.log(queryUrl);
+
+      
+    
+        
                 console.log("-----------------------------------------------------------------------------")
                 console.log("    The Artist " + appSearch + " Will be performing on the following dates and venues:")
                 console.log("-----------------------------------------------------------------------------")
                 console.log("                ***--------EVENT INFO--------***");  
-                fs.appendFileSync("random.txt", "**********EVENT INFO*********\n");
-                console.log(i);
-                fs.appendFileSync("random.txt", i+"\n");
-                console.log("              | Name of the Venue: " + concerts[i].venue.name + " |");
-                fs.appendFileSync("random.txt",      "Name of the Venue: " + concerts[i].venue.name+"\n");
-                console.log("              | Venue Location: " +  concerts[i].venue.city + " |");
-                fs.appendFileSync("random.txt",      "Venue Location: " +  concerts[i].venue.city+"\n");
-                console.log("              | Date of the Event: " +  concerts[i].datetime + " |");
-                fs.appendFileSync("random.txt",      "Date of the Event: " +  concerts[i].datetime+"\n");
+                // fs.appendFileSync("random.txt", "**********EVENT INFO*********\n");
+                console.log();
+                // fs.appendFileSync("random.txt", +"\n");
+                 console.log("              | Ticket Status: " + jsonData.offers.type + " |");
+                // fs.appendFileSync("random.txt", +"\n");
+                console.log("              | Name of the Venue: " + jsonData.venue.name + " |");
+                // fs.appendFileSync("random.txt",      "Name of the Venue: " + jsonData.venue.name+"\n");
+                console.log("              | Venue Location: " +  jsonData.venue.city + " |");
+                // fs.appendFileSync("random.txt",      "Venue Location: " +  jsonData.venue.city+"\n");
+                console.log("              | Date of the Event: " +  jsonData.datetime + " |");
+                // fs.appendFileSync("random.txt",      "Date of the Event: " +  jsonData.datetime+"\n");
+                console.log("              | Description of the Event: " +  jsonData.description + " |");
+                // fs.appendFileSync("random.txt",      "Description of Event: " +  jsonData.description+"\n");
                 
                 console.log("                                                                       ");
-                fs.appendFileSync("random.txt", "*****************************"+"\n");
-            }
-        } else{
-          console.log('Error occurred.');
+                // fs.appendFileSync("random.txt", "*****************************"+"\n");
+                 
         }
-    });}
+        );
+    }
+
+    var getOMBD = function(getmovie) {
+        if(getmovie === undefined){
+            getmovie = "Mr. Nobody";
+        }
+
+    }
+
+// ------------------------------------------------------------------------------------------------
+
+
+    function getOMBD(response){
+        var queryUrl1 = "http://www.omdbapi.com/?t=" + appSearch +  "&y=&plot=full&tomatoes=true&apikey=e333fe52"
+        
+        axios.get(queryUrl1).then(
+            function(response) {
+              var jsonData = response.data;
+                
+            console.log("-------------------------");
+            console.log("  Title: " + jsonData.Title);
+            console.log("-------------------------");
+            console.log(" Year: " + jsonData.Year + '\n');
+            console.log(" Rated: " + jsonData.Rated + '\n');
+            console.log(" IMDB Rating: " + jsonData.imdbRating + '\n');
+            console.log(" Country: " + jsonData.Country+ '\n');
+            console.log(" Language: " + jsonData.Language+ '\n');
+            console.log(" Plot: " + jsonData.Plot);
+            console.log(" Actors: " + jsonData.Actors);
+            console.log("-------------------------");
+            console.log("  Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+            console.log("-------------------------");
+    }
+  );
+};
+
+
+
+
+
+
 
 
 
